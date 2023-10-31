29126e238051f08f61bc3811737cf41fa48a7751
Bartosz <121699921+BartoszTanski@users.noreply.github.com>
2023-10-31 14:52:27 +0100

    Update EmailServiceImpl.java

--- notificationservice/src/main/java/com/bartosztanski/notificationservice/service/EmailServiceImpl.java
+++ notificationservice/src/main/java/com/bartosztanski/notificationservice/service/EmailServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
